cd8ae37f78e49acf2f4c92198f958b6bb238633b
milan11rakhal <milan11rakhal@gmail.com>
2024-11-21 01:37:04 +0545

    adding repo

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = [
+    {name: 'Milan Rakhal Magar', email:'milan11rakhal@gmail.com', contact: "9860777508"},
+    {name: 'Ishor Rakhal Magar', email:'milan11rakhal@gmail.com', contact: "9860777508"},
+    {name: 'Bishnu', email:'milan11rakhal@gmail.com', contact: "9860777508"},
+    {name: 'Sharthak', email:'milan11rakhal@gmail.com', contact: "9860777508"},
+    {name: 'kamal', email:'milan11rakhal@gmail.com', contact: "9860777508"},
+    {name: 'Summin', email:'milan11rakhal@gmail.com', contact: "9860777508"},
+    {name: 'Roshan', email:'milan11rakhal@gmail.com', contact: "9860777508"},
+    {name: 'Binita', email:'milan11rakhal@gmail.com', contact: "9860777508"},
+    {name: 'Sunil', email:'milan11rakhal@gmail.com', contact: "9860777508"},
+
+]
+module.exports = data;
