c7c48edb2d84198254322f873909ff5f2f3e25f0
milan11rakhal <milan11rakhal@gmail.com>
2024-12-13 04:10:45 +0545

    adding mongoose

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+const mongoose = require('mongoose'); 
 
-const dbConnect = require('./mongodb')
+const main = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/e-com', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }); 
 
-const main = async ()=>{
-    let data = await dbConnect();
-    data = await data.find().toArray();
-    console.log(data)
-}
+        const ProductSchema = new mongoose.Schema({
+            name: String,
+            price: Number,
+        });
 
-main()
+        const ProductModel = mongoose.model('products', ProductSchema);
 
+        let data = new ProductModel({ name: 'm 10', price: 1000 });
+        let result = await data.save();
+        console.log(result);
+    } catch (err) {
+        console.error('Error connecting to the database:', err.message);
+    } finally {
+        await mongoose.connection.close();
+    }
+};
 
+main();
